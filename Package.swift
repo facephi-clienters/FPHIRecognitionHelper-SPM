eb741a9d078975a6781e48362508b17625891ff0
Jose Antonio <joseantoniojuan@facephi.com>
2026-04-23 16:55:08 +0200

    New version: 6.1.3

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FPHIRecognitionHelper-SPM",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "FPHIRecognitionHelper-SPM",
+            targets: ["FPHIRecognitionHelper-SPM", "FPHIRecognitionHelper"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "git@github.com:facephi-clienters/FPBRecognition-SPM.git", .exactItem("6.1.3")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "FPHIRecognitionHelper-SPM",
+            dependencies: [
+                "FPBRecognition-SPM", "FPHIRecognitionHelper"]),
+
+         .binaryTarget(name: "FPHIRecognitionHelper", url: "https://facephicorp.jfrog.io/artifactory/spm-pro-fphi/WIDGET/DocumentReader/FPHIRecognitionHelper/6.1.3/FPHIRecognitionHelper.zip",
+            checksum: "cd76f14489ae0d3cf869bc45c31755db60322e7ba4d9b011366472b59aaabc8a") 
+    ]
+)
